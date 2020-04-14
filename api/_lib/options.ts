6ea9ef891a8888680a75b496ed8cda5df98ae820
Mike Barkmin <mike@barkmin.eu>
2020-04-14 22:08:00 +0200

    Add path for chrome on linux (#111)

--- api/_lib/options.ts
+++ api/_lib/options.ts
@@ -2,7 +2,9 @@ import chrome from 'chrome-aws-lambda'
 const exePath =
   process.platform === 'win32'
     ? 'C:\\Program Files (x86)\\Google\\Chrome\\Application\\chrome.exe'
-    : '/Applications/Google Chrome.app/Contents/MacOS/Google Chrome'
+    : process.platform === 'linux'
+      ? '/usr/bin/google-chrome'
+      : '/Applications/Google Chrome.app/Contents/MacOS/Google Chrome'
 
 interface Options {
   args: string[]
